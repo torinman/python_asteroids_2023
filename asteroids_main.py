05b0556d6112e52689e6104f9688465a89fda45c
torinman <torin@theblorenge.co.uk>
2023-09-18 18:49:51 +1200

    Add main imports

--- asteroids_main.py
+++ asteroids_main.py
@@ -30,3 +30,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see  <https://www.gnu.org/licenses/>.
 
+import pygame
+import math
+import collision
+import random
