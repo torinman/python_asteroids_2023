da5b2f7feb24b24d58b6f83f276ff0bafc45e0b4
torinman <torin@theblorenge.co.uk>
2023-11-14 17:25:36 +1300

    invert colours in asteroids_random_test

--- tests.py
+++ tests.py
@@ -8,9 +8,9 @@ def asteroids_random_test():
     asteroid = classes.Asteroid()
     asteroid.location = (constants.ASTEROID_SCALES[0]*1.5, constants.ASTEROID_SCALES[0]*1.5)
     screen = pygame.display.set_mode((constants.ASTEROID_SCALES[0]*3, constants.ASTEROID_SCALES[0]*3))
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
     for line in asteroid.location_lines():
-        pygame.draw.line(screen, (0, 0, 0), line[0], line[1])
+        pygame.draw.line(screen, (255, 255, 255), line[0], line[1])
     done = False
     while not done:
         for event in pygame.event.get():
